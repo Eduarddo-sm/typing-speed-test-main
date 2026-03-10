278f1ccac026e3f2ef44ed0fdf3c4d9332b4e57f
Eduardo Windows <eduardosilvamesquita61@gmail.com>
2026-03-09 22:57:45 -0300

    added render text

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,35 +1,48 @@
 import words from "./data.json" with {type: "json"};
+const difficulties = document.querySelectorAll(".button__difficulty");
+const mode = document.querySelectorAll(".button__mode");
+const typeSettings = {
+    diffculty: "easy",
+    mode: "timed"
+}
 
-const difficulty = document.querySelectorAll('.button__difficulty');
-const mode = document.querySelectorAll('.button__mode');
-
-
-
-//difficulty é uma array que tem todos os botões de dificuldade. A gente chama esse array de botoês e para cada botão, utilizando um forEach, que fisga todos os botões, então a gente declara para cada botão dentro de difficulty, um evento de escuto utilizando colchetes
-difficulty.forEach(button => {
-    //dentro do forEach, definimos cada botão com a var button, e então criamos para cada button um evento de escuta, ou seja, cada botão ao ser  clicado fará a mesma ação, que será remover a classe active de todos os botões conforme difficulty, e de´pos adicionar no button do difficulty clicado a classe active.
-    button.addEventListener('click', () =>{
-        difficulty.forEach(btn => btn.classList.remove('active'));
+difficulties.forEach(button => {
+    button.addEventListener('click', ()=>{
+        difficulties.forEach(btn => btn.classList.remove('active'));
         button.classList.add('active');
 
-        const target = button.getAttribute('data-tab');
-        getDifficulty(target);
+        typeSettings.diffculty = button.getAttribute("data-tab");
+        startTyping();
     })
-
-
 })
 
 mode.forEach(button => {
-    button.addEventListener('click', () =>{
+    button.addEventListener('click', ()=>{
         mode.forEach(btn => btn.classList.remove('active'));
         button.classList.add('active');
+
+        typeSettings.mode = button.getAttribute("data-tab");
     })
 })
 
+function startTyping(){
+    const diffculty = typeSettings.diffculty;
+    const mode = typeSettings.mode;
+    const passage = words[diffculty];
+    const randomIndex = Math.floor(Math.random() * passage.length);
+    const text = passage[randomIndex].text;
 
-function getDifficulty(target) {
-    console.log(target);
-    target = `${target}-${1}`;
-    console.log(target);
-    console.log(words[target])
+    renderText(text);
+}
+
+
+function renderText(text){
+    
+    const container = document.getElementById("card__text");
+    container.innerHTML = "";
+    text.split("").forEach(letter => {
+        const span = document.createElement("span");
+        span.textContent = letter;
+        container.appendChild(span);
+    })
 }
